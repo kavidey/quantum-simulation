47a3da03e58f3dbb24f364cef719d9963ced90e8
KaviMD <kaviasher@gmail.com>
2017-06-13 17:44:56 -0400

    Grovers Algorithm added

---
+++ Quantum-Computation/QuantumGates.py
@@ -0,0 +1,120 @@
+# Quantum Gates Library
+
+# Import necessary libraries
+import math
+
+# Function to flip a bit
+def flipBit(num, bit):
+	# num -- the number whose bit gets flipped
+	# bit -- the bit that gets flipped
+	mask = 1 << (bit)	# generate a mask for bit
+	return num ^ mask # XOR the mask and num
+
+# Flip 2 items in an array,
+# 1st specified by loc
+# 2nd by flipBit(loc, bit)
+def swap(prob, loc, bit):
+	# prob -- array of data
+	# loc -- index of 1st item
+	# bit -- used to calculate index of 2nd item
+	tmp = prob[loc] # store prob[loc] in tmp
+	invert = flipBit(loc, bit) # find the inverted location
+	prob[loc] = prob[invert] # set prob[invert] to prob[loc] 
+	prob[invert] = tmp # set prob[invert] to tmp (the old prob[loc])
+	return prob
+
+# Function used in Grovers Algorithm 
+def F(qi):
+	# qi -- value to check
+	if qi == 2: # check if qi is 2
+		return 1 # if so return 1
+	return 0 # otherwise return 0
+	
+# Function that rounds all items in an array 
+# to the nearist simple decimia
+def round(prob):
+	# prob -- array to be rounded
+	for i in range(0, len(prob)): # loop through all items in array
+		if prob[i] < 0.000001 and prob[i] > -0.000001: # check if item is between 0.000001 and -0.000001
+			prob[i] = 0 # if so set item to 0
+		elif prob[i] < 1.000001 and prob[i] > 0.999999: # check if item is between 1.000001 and 0.999999
+			prob[i] = 1 # if so set item to 1
+		elif prob[i] < 0.5 and prob[i] > 0.499999: # check if item is between 0.5 and 0.499999
+			prob[i] = 0.5 # if so set item to 0.5
+	return prob # return the new array
+	
+# NOT gate (flips the probabilities of all pairs)
+def NOT(prob, qi):
+	# prob -- array of data
+	# qi -- qubit to be NOT'd
+	# maski -- binary mask used to only apply NOT's when the control bit is 0
+	maski = 1 << qi
+	for i in xrange(0, len(prob)): # loop through all inputs
+		if i & maski == 0: # check whether bit qi in i is 0
+			prob = swap(prob, i, qi) # swap items in prob
+	return prob # return the new array
+
+# CNOT gate (flips the probabilities of a pair if a selected bit is 1)
+def CNOT(prob, qi, qj):
+	# prob -- array of data
+	# qi -- controller bit
+	# qj -- qubit to be NOT'd
+	# maski -- binary mask used to only apply CNOT's when the control bit is 1
+	maski = 1 << qi
+	for i in xrange(0, len(prob)): # loop through all inputs
+		if i & maski != 0: # check if control bit (qi) is one
+			prob = swap(prob, i, qj) # if so swap that bit and its inverse
+	return prob # return the new array
+
+# CCNOT gate (flips the probabilities of a pair if the selected bits are both 1)
+def CCNOT(prob, qi, qj, qk):
+	# prob -- array of data
+	# qi -- 1st controller bit
+	# qj -- 2nd controller bit
+	# qk -- qubit to be NOT'd
+	# maski -- binary mask used to only apply CCNOT's when the control bits are 1
+	# maskj -- binary mask used to only apply CCNOT's when the control bits are 1
+	maski = 1 << qi
+	maskj = 1 << qj
+	for i in xrange(0, len(prob)): # loop through all inputs
+		if i & maski != 0 and i & maskj != 0: # check if control bits qi and qj are one
+			if i & maskj == 0: # check whether bit qi in i is 0
+				prob = swap(prob, i, qk) # if so swap that bit and its inverse
+	return prob# return the new array
+	
+# Hadamard gate (like a COIN gate but with negative amplitudes)
+def Hadamard(prob, qi):
+	# prob -- array of data
+	# maski -- binary mask used to only apply Hadamards's when the control bit is 0
+	# sqrtTwo -- variable to store the square root of 2
+	# qi -- qbit to apply the Hadamard gate to
+	sqrtTwo = 1.4142135623730951
+	maski = 1 << qi
+	for i in range(0, len(prob)): # loop through all inputs
+		if i & maski == 0: # check if control bit is 0
+			flip = flipBit(i, qi) # get the flipped location
+			a = prob[i] # store prob[i] in a
+			b = prob[flip] # store prob[flip] in b
+			prob[i] = (1/sqrtTwo) * (a+b) # set prob[i] to 1/sqrt2 * (a+b)
+			prob[flip] = (1/sqrtTwo) * (a-b )# set prob[flip] to 1/sqrt2 * (a-b)
+	return round(prob) 
+	
+def ZNOT(prob):
+	for i in range(1, len(prob)):
+		prob[i] = prob[i]*-1
+	return prob
+	
+def Oracle(prob):
+	for i in range(0, len(prob)):
+		if F(i) == 1:
+			prob[i] = prob[i] * -1
+	return prob
+	
+def GroverDiffusion(prob):
+	for i in range(0, int(math.log(len(prob), 2))):
+		Hadamard(prob, i)
+	ZNOT(prob)
+	for i in range(0, int(math.log(len(prob), 2))):
+		Hadamard(prob, i)
+	return prob
+	
---
+++ Quantum-Computation/main.py
@@ -0,0 +1,92 @@
+# Program execution file
+
+# Import Quantum Gates
+import QuantumGates
+
+# Import Random
+import random
+
+# Read Program File
+file = open('Program.txt', 'r')
+
+# Make it an Array
+program = file.readlines()
+
+# Initalize QBit array and data storage array
+QBits = [0] * (2**int(program[0]))
+Data = [0] * (2**int(program[0]))
+
+# Set first item to 1
+QBits[0] = 1
+
+# Set the deafault output as an array of probabilities
+outputData = False
+
+# Set the deafault number of times to loop when outputting actual data
+times = 0
+
+checkFirst = True
+
+for i in xrange(1, len(program)): # loop all rows in 'Program.txt'
+  command = program[i].split() # split row into array at spaces
+  
+  if i == 1:
+  	try: # try
+  		int(command[0]) # check if turning command[0] into an int returns an error
+  		outputData =  True # change ouptutData to True (output result of running simulation)
+  		times = int(command[0]) # set times to the value of command[0]
+  	except ValueError: # except
+  		outputData =  False # keep output data as false
+       
+  if command[0] == '#': # check if row is comment
+    print 'comment: ' + str(program[i]) # if so print the comment
+    
+  elif command[0] == 'NOT': # check if row is a NOT gate
+    QBits = QuantumGates.NOT(QBits,int(command[1])) # if so apply the NOT gate
+    
+  elif command[0] == 'CNOT': # check if row is a CNOT gate
+    QBits = QuantumGates.CNOT(QBits, int(command[1]), int(command[2])) # if so apply the CNOT gate
+    
+  elif command[0] == 'CCNOT': # check if row is a CCNOT gate
+    QBits = QuantumGates.CCNOT(QBits, int(command[1]), int(command[2]), int(command[3])) # if so apply the CCNOT gate
+    
+  elif command[0] == 'H' or command[0] == 'Hadamard': # check if row is a Hadamard gate
+    QBits = QuantumGates.Hadamard(QBits, int(command[1])) # if so apply the Hadamard gate
+    
+  elif command[0] == 'ZNOT': # check if row is a ZNOT gate
+  	QBits = QuantumGates.ZNOT(QBits) # if so apply the ZNOT gate
+  	
+  elif command[0] == 'Oracle': # check if row is a Oracle gate
+  	QBits = QuantumGates.Oracle(QBits) # if so apply the Oracle gate
+  
+  elif command[0] == 'GroverDiffusion' or command[0] == 'GD': # check if row is Grover Diffusion
+  	QBits = QuantumGates.GroverDiffusion(QBits) # if so apply the Grover Diffusion
+  	checkFirst = False
+
+if outputData == False:
+	squared = []
+	for i in range(0, len(QBits)):
+		squared.append(QBits[i]**2)
+	print squared
+else:
+	for i in range(0, times):
+		test = random.uniform(0.0,1.0)
+		if checkFirst == True:
+			prob = QBits[0]**2
+			for i in range(0, len(QBits)):
+				if test < prob and test > (prob - (QBits[i]**2)):
+					Data[i] = Data[i]+1
+				if i != len(QBits)-1:
+					prob = prob + (QBits[i+1]**2)
+				if QBits[i] == 0:
+					Data[i] = 0
+		else:	
+			prob = QBits[1]**2
+			for i in range(1, len(QBits)):
+				if test < prob and test > (prob - (QBits[i]**2)):
+					Data[i] = Data[i]+1
+				if i < len(QBits)-1:
+					prob = prob + (QBits[i+1]**2)
+				if QBits[i] == 0:
+					Data[i] = 0
+	print Data
